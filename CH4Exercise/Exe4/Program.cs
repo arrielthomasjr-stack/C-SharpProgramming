2c6d820e6fe171ca7bd51d3881bd2a57b4d32f33
Arrie Lewis Thomas Jr <owathomasjr@Arries-MacBook-Pro.local>
2026-09-09 17:38:20 -0400

    Add chapter 4 C# exercises

---
+++ CH4Exercise/Exe4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+class Hurricane
+{
+    static void Main()
+    {
+        Console.Write("Enter the wind speed in mph: ");
+        int windSpeed = Convert.ToInt32(Console.ReadLine());
+
+        if (windSpeed < 74)
+        {
+            Console.WriteLine("This is not a hurricane.");
+        }
+        else if (windSpeed >= 74 && windSpeed <= 95)
+        {
+            Console.WriteLine("The hurricane is a Category 1 hurricane.");
+        }
+        else if (windSpeed >= 96 && windSpeed <= 110)
+        {
+            Console.WriteLine("The hurricane is a Category 2 hurricane.");
+        }
+        else if (windSpeed >= 111 && windSpeed <= 129)
+        {
+            Console.WriteLine("The hurricane is a Category 3 hurricane.");
+        }
+        else if (windSpeed >= 130 && windSpeed <= 156)
+        {
+            Console.WriteLine("The hurricane is a Category 4 hurricane.");
+        }
+        else
+        {
+            Console.WriteLine("The hurricane is a Category 5 hurricane.");
+        }
+
+
+    }
+}
---
+++ CH4Exercise/Exe6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+/*Account numbers sometimes contain a check digit that is the result of a mathematical
+calculation. The inclusion of the digit in an account number helps ascertain whether
+the number is a valid one. Write an application named CheckDigit that asks a user
+to enter a four-digit account number and determines whether it is a valid number.
+The number is valid if the fourth digit is the remainder when the number represented
+by the first three digits of the four-digit number is divided by 7. For example, 7770 is
+valid, because 0 is the remainder when 777 is divided by 7.*/
+class CheckDigit
+{
+    static void Main()
+    {
+        Console.Write("Enter four digit account number: ");
+        int accountNumber = Convert.ToInt32(Console.ReadLine());
+
+        int firstThreeDigits = accountNumber / 10; // Get the first three digits
+        int checkDigit = accountNumber % 10; // Get the fourth digit
+
+        if (checkDigit == (firstThreeDigits % 7))
+        {
+            Console.WriteLine("The account number is valid.");
+        }
+        else
+        {
+            Console.WriteLine("The account number is not valid.");
+        }
+    }
+}
